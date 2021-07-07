92a729a550d05aa0e9b64931602fadbced063e3b
kmouofo <86736515+kmouofo@users.noreply.github.com>
2021-07-07 12:52:48 -0400

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,4 +1,14 @@
- $("#fsCell112356591 > input").autocomplete({
+ $( function() {
+    var availableTags = [
+      "A T Still University-Health",
+"A.R.T. Institute of Washington, Inc.",
+"AFI Silver Theatre and Cultural Center",
+"AIU Online",
+"ASA College",
+"Abilene Christian University",
+"Abraham Baldwin Agricultural College"];
+   $("#fsCell112356591 > input").autocomplete({
     source: availableTags,
       minLength : 3
+});
 });
